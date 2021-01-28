2180f4ce7c9f93979fd5e1c6cf0ac764796bbcf4
Yoga Subagja <42853517+Sality32@users.noreply.github.com>
2021-01-28 22:49:08 +0700

    Add files via upload

---
+++ Hit the Lottery.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int n,ans=0;
+	scanf("%d",&n);
+	while(n>0){
+		if(n>=100){
+			n-=100;
+			ans++;
+		}else if(n>=20){
+			n-=20;
+			ans++;
+		}else if(n>=10){
+			n-=10;
+			ans++;
+		}else if(n>=5){
+			n-=5;
+			ans++;
+		}else {
+			n-=1;
+			ans++;
+		}
+	}
+	printf("%d",ans);
+}
